436e4e5de7f79e24f2b46bfa4d8d98e503546595
ivanistrator <peregoedov20@gmail.com>
2024-08-13 04:52:05 +0700

    Post -> Put

--- src/main/java/com/pepeg/application/controller/OrganizerController.java
+++ src/main/java/com/pepeg/application/controller/OrganizerController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,7 +44,7 @@ public class OrganizerController {
 
     }
 
-    @PostMapping("events/edit")
+    @PutMapping("events/edit")
     public Event edit(@RequestBody Event event, @RequestParam Long id) {
         return eventService.editEvent(event, id);
     }
